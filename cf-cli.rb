1fbff9e41438ad92989e208972fbb69238618f5d
Kris Hicks <khicks@pivotal.io>
2015-12-23 09:26:08 -0800

    Initial commit

---
+++ cf-cli.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class CfCli < Formula
+  homepage 'https://github.com/cloudfoundry/cli'
+  head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
+  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.14.1&source=homebrew'
+  version '6.14.1'
+  sha1 '914b60aedc4515f3770c46e4bbbf322227dad417'
+
+  depends_on :arch => :x86_64
+
+  def install
+    bin.install 'cf'
+  end
+
+  test do
+    system "#{bin}/cf"
+  end
+end
