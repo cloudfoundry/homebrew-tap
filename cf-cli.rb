1040c0e21872b2b2035a3b351a38037dbf4f40ac
Kris Hicks <khicks@pivotal.io>
2016-01-13 15:19:33 +1100

    Update cf-cli to 6.15.0
    Signed-off-by: Muhammad Altaf <malik.altaf@gmail.com>

--- cf-cli.rb
+++ cf-cli.rb
@@ -3,9 +3,9 @@ require 'formula'
 class CfCli < Formula
   homepage 'https://github.com/cloudfoundry/cli'
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
-  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.14.1&source=homebrew'
-  version '6.14.1'
-  sha1 '914b60aedc4515f3770c46e4bbbf322227dad417'
+  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.15.0&source=homebrew'
+  version '6.15.0'
+  sha1 '1179c8e60395bb8a6b6bc5eb2880a3470106dec6'
 
   depends_on :arch => :x86_64
 
