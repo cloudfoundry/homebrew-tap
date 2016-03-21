38083a70d06d1b4315d9f664a5a51205c142a95a
Anand Gaitonde <XenoPhex@users.noreply.github.com>
2016-03-21 16:55:19 -0700

    Add conflict with "caskroom/cask/cloudfoundry-cli"

--- cf-cli.rb
+++ cf-cli.rb
@@ -10,6 +10,7 @@ class CfCli < Formula
   depends_on :arch => :x86_64
 
   conflicts_with "pivotal/tap/cloudfoundry-cli", :because => "the Pivotal tap ships an older cli distribution"
+  conflicts_with "caskroom/cask/cloudfoundry-cli", :because => "the caskroom tap is not the official distribution"
 
   def install
     bin.install 'cf'
