b6c80be0f53299e02d10d85812d0df1606280049
Marco Voelz <marco.voelz@sap.com>
2016-07-05 14:21:22 +0200

    Refactor formula to have version in one place only

--- bosh-init.rb
+++ bosh-init.rb
@@ -1,8 +1,8 @@
 class BoshInit < Formula
   desc "creates and updates the Director VM"
   homepage "https://github.com/cloudfoundry/bosh-init"
-  url "https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.95-darwin-amd64"
   version "0.0.95"
+  url "https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-#{version}-darwin-amd64"
   sha256 "312b503bf545ef40cb72470392f9d356364a2c49441b24b1ec7f3bcdd48416a3"
 
   depends_on :arch => :x86_64
@@ -10,7 +10,7 @@ class BoshInit < Formula
   conflicts_with "pivotal/tap/bosh-init", :because => "the Pivotal tap ships an older bosh-init version and is outdated"
 
   def install
-    bin.install "bosh-init-0.0.95-darwin-amd64" => "bosh-init"
+    bin.install "bosh-init-#{version}-darwin-amd64" => "bosh-init"
   end
 
   test do
