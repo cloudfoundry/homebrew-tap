ef64510e1a361fcc2c4427286bdf86563903910a
Concourse CI <cf-cli-eng@pivotal.io>
2016-08-19 20:44:11 +0000

    Release 6.21.1

--- cf-cli.rb
+++ cf-cli.rb
@@ -5,7 +5,7 @@ class CfCli < Formula
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
   url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.21.1&source=homebrew'
   version '6.21.1'
-  sha256 '2922ce5f617fbd6395484b56d07d0734cccec4d68b307d63ac3aa59fd3c99a88'
+  sha256 '347c70c4f537dff4315cbb42c33e8707a5ba180be74bd8a4a7621d7d56b6258e'
 
   depends_on :arch => :x86_64
 
