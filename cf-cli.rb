321b7d0ea7a083e90df81eb1eb3efab6862af05d
Luan Santos <lsantos@pivotal.io>
2016-08-22 18:10:12 -0700

    correct sha
    [#128748851]
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- cf-cli.rb
+++ cf-cli.rb
@@ -5,7 +5,7 @@ class CfCli < Formula
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
   url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.21.1&source=homebrew'
   version '6.21.1'
-  sha256 '65fbc02b8a10579c2f8d7bfd0afd3c89734eff9fb6ff4867be0632209089e9cc'
+  sha256 '0faca62b5983260a371a08af8c5cb73d66ca324da16a4225789f2a479107ff18'
 
   depends_on :arch => :x86_64
 
