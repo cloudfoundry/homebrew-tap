89b621969b00ab0401ab14df790bdac693fc3cc7
Marco Voelz <voelzmo@users.noreply.github.com>
2016-10-13 08:41:27 +0200

    Update bosh-init to 0.0.98
    Now really built with golang 1.7.1 to work on macOS sierra.

--- bosh-init.rb
+++ bosh-init.rb
@@ -1,9 +1,9 @@
 class BoshInit < Formula
   desc "creates and updates the Director VM"
   homepage "https://github.com/cloudfoundry/bosh-init"
-  version "0.0.97"
+  version "0.0.98"
   url "https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-#{version}-darwin-amd64"
-  sha256 "3b2d18265f2384cc907bd4b75cb27a2af3e29d23cc3ca221ee10ba21c020be19"
+  sha256 "05f3b10c9adb78d45c367b902bec37e8379c5a044d45fde55d0a42b916a8eb40"
 
   depends_on :arch => :x86_64
 
