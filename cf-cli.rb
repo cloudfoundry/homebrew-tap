48da79aac5da7f3cf9681cc6ce8cc0918b3ba45b
Concourse CI <cf-cli-eng@pivotal.io>
2017-01-13 18:03:24 +0000

    Release 6.23.1

--- cf-cli.rb
+++ cf-cli.rb
@@ -3,9 +3,9 @@ require 'formula'
 class CfCli < Formula
   homepage 'https://code.cloudfoundry.org/cli'
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
-  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.23.0&source=homebrew'
-  version '6.23.0'
-  sha256 '2da08e15223147d87caf51bc7fff6eb224c3a194d57052e69f3bc2ba05209541'
+  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.23.1&source=homebrew'
+  version '6.23.1'
+  sha256 'aee12111e7403f97270c59bd2f05551f6b23a30729843ce92a5422d0f319ab60'
 
   depends_on :arch => :x86_64
 
@@ -14,6 +14,8 @@ class CfCli < Formula
 
   def install
     bin.install 'cf'
+    doc.install 'LICENSE'
+    doc.install 'NOTICE'
   end
 
   test do
