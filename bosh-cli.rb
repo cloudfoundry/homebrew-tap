c7b59c85c373c4b237cfa52b72ef0d2253803f97
Marco Voelz <voelzmo@users.noreply.github.com>
2017-01-19 20:13:48 +0100

    Bump bosh-cli to 0.0.147

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -1,8 +1,8 @@
 class BoshCli < Formula
   desc "The new golang BOSH cli (alpha)"
-  version "0.0.67"
+  version "0.0.147"
   url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-  sha256 "85d36305c3d9529a8e4756bdf28dfd3c089c787b331ad8afdb673752107e2f8f"
+  sha256 "60eb9269ac02ae431264887be974a4818deabb9932f1fc14d7a9841045c90da1"
 
   depends_on :arch => :x86_64
 
