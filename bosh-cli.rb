688930fb86a03410ff08e9e1f467471f93770135
BOSH CI <cf-bosh-eng@pivotal.io>
2017-01-27 19:13:49 +0000

    Release bosh-cli 0.0.149

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -1,9 +1,9 @@
 class BoshCli < Formula
   desc "New BOSH CLI (beta)"
   homepage "https://bosh.io/docs/cli-v2.html"
-  version "0.0.147"
+  version "0.0.149"
   url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-  sha256 "60eb9269ac02ae431264887be974a4818deabb9932f1fc14d7a9841045c90da1"
+  sha256 "b114afa7ac2e60196b890447e01a3d58c29bee48072c0a21e8abc6de5150eaab"
 
   depends_on :arch => :x86_64
 
