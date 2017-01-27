2b2865859d3f5e5dcfe6acf19c5f6d1ab2885773
BOSH CI <cf-bosh-eng@pivotal.io>
2017-01-27 21:28:16 +0000

    Release bosh-cli 0.0.150

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -1,14 +1,27 @@
 class BoshCli < Formula
   desc "New BOSH CLI (beta)"
   homepage "https://bosh.io/docs/cli-v2.html"
-  version "0.0.149"
+  version "0.0.150"
   url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-  sha256 "b114afa7ac2e60196b890447e01a3d58c29bee48072c0a21e8abc6de5150eaab"
+  sha256 "d56e066bdb30303c2dcadec68e0d0c613ba28271b82c1e049671ef8fd1524be1"
 
   depends_on :arch => :x86_64
 
   def install
     bin.install "bosh-cli-#{version}-darwin-amd64" => "gosh"
+    (bash_completion/"bosh-cli").write <<-completion
+      _gosh() {
+          # All arguments except the first one
+          args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
+          # Only split on newlines
+          local IFS=$'\n'
+          # Call completion (note that the first element of COMP_WORDS is
+          # the executable itself)
+          COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} "${args[@]}"))
+          return 0
+      }
+      complete -F _gosh gosh
+    completion
   end
 
   test do
