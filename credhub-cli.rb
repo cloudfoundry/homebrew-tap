158a5f234fba0625c19eb4214cbb543a00d6dad4
Dan Jahner <djahner@pivotal.io>
2017-04-29 11:34:50 -0700

    Release credhub-cli 0.7.0

--- credhub-cli.rb
+++ credhub-cli.rb
@@ -1,9 +1,9 @@
 class CredhubCli < Formula
   desc "CredHub CLI"
   homepage "https://github.com/cloudfoundry-incubator/credhub-cli"
-  version "0.6.0"
-  url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/0.6.0/credhub-darwin-0.6.0.tgz"
-  sha256 "24c478b7edb32bc9366be982d740acdd98615aac39b8d61795213141641a7a52"
+  version "0.7.0"
+  url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/0.7.0/credhub-darwin-0.7.0.tgz"
+  sha256 "f6b25231e414a038dffb368142664c0fc22f1c37fbeaeee7227a7b1da4be9d46"
 
   depends_on :arch => :x86_64
 
