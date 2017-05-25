28ae28e4a142a1498124e425ab6b7569a0b59417
Genevieve LEsperance <glesperance@pivotal.io>
2017-05-25 14:10:06 -0700

    Bump bbl.
    Signed-off-by: Christian Ang <cang@pivotal.io>

--- bbl.rb
+++ bbl.rb
@@ -1,9 +1,9 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
   homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v3.1.0"
+  version "v3.2.1"
   url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
-  sha256 "cb9d2e3d0bd09ccdcff9bbd7bbc2f204ffae8464b114a376d2eab2e08921ad94"
+  sha256 "8c71bc2bb43a58232a644fc2f26768942c5d17f0fe1e904871d73c7c1b5baa44"
 
   depends_on :arch => :x86_64
   depends_on "terraform" => "0.9.4"
