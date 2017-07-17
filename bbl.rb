0a99a76c6ff688ec5cceab838048d9e15a3e5d6d
Genevieve LEsperance <glesperance@pivotal.io>
2017-07-17 10:13:27 -0700

    Bump bbl to 3.2.6

--- bbl.rb
+++ bbl.rb
@@ -1,9 +1,9 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
   homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v3.2.4"
+  version "v3.2.6"
   url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
-  sha256 "fc926f1694312188d9ee0e8995b3f8cf67e4509b8bae30e3782fe67c719fa108"
+  sha256 "92a2d392f8ea11f722d514eb644465b1c68db60769bf524a6ee7c6f05cf42b5d"
 
   depends_on :arch => :x86_64
   depends_on "terraform" => "0.9.8"
