49d89a33ecaa8b3cfbac0ff288e1d055b2273276
Nick Mahoney <nmahoney@pivotal.io>
2017-08-10 16:40:03 -0700

    Revert "Release bbl 4.0.1"
    This reverts commit 7941a61bffa83d08d5afb30b7513e01c684b718b.
    
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

--- bbl.rb
+++ bbl.rb
@@ -1,12 +1,12 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
   homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v4.0.1"
+  version "v4.0.0"
   url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
   sha256 "6771463dfeec5d1658e7749f45524bce475219c0104abea482149f7ae25eb4e5"
 
   depends_on :arch => :x86_64
-  depends_on "terraform" => "0.10.0"
+  depends_on "terraform" => "0.9.11"
   depends_on "cloudfoundry/tap/bosh-cli" => "2.0.29"
 
   def install
