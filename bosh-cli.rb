c5676cdf727f748f7bda2e513343246cb828c14b
BOSH CI <cf-bosh-eng@pivotal.io>
2017-08-14 23:00:18 +0000

    Release bosh-cli 2.0.31

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -1,16 +1,16 @@
 class BoshCli < Formula
   desc "New BOSH CLI (beta)"
   homepage "https://bosh.io/docs/cli-v2.html"
-  version "2.0.30"
+  version "2.0.31"
   url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-  sha256 "dcd623735a6d1893a322060878d3d6801200ea75fcd6dea5e1d8507711c1ea7d"
+  sha256 "d9339d2e5605530a1bbe9dab5278257efdc7b88b5d109aa91d39246d1587ceb1"
 
   depends_on :arch => :x86_64
 
-  option "without-bosh2", "Don't rename binary to 'bosh2'. Useful if the old Ruby CLI is not needed."
+  option "with-bosh2", "Rename binary to 'bosh2'. Useful if the old Ruby CLI is needed."
 
   def install
-    binary_name = build.without?("bosh2") ? "bosh" : "bosh2"
+    binary_name = build.with?("bosh2") ? "bosh2" : "bosh"
     bin.install "bosh-cli-#{version}-darwin-amd64" => binary_name
   end
 
