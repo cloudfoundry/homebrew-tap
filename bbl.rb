b60473085fdae2d7b8a298d5ced883f00c49e98c
Vlad Stoian <vstoian@pivotal.io>
2017-10-05 13:24:01 +0100

    Use version variable and add sha256 to all formulas

--- bbl.rb
+++ bbl.rb
@@ -1,13 +1,13 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
-  homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v4.10.3"
+  homepage 'https://github.com/cloudfoundry/bosh-bootloader'
+  version 'v4.10.3'
   if OS.mac?
     url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
-    sha256 ""
+    sha256 'd26d512738d09d1e42b1faa04cda40d9773e5f08b23b6c78f493d94931d0cd6f'
   elsif OS.linux?
     url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_linux_x86-64"
-    sha256 ""
+    sha256 'd3de414360810c9d616fbe1127dd3590a2e0c7a81df5571f59a66cc04b12441f'
   end
 
   depends_on :arch => :x86_64
--- cf-cli.rb
+++ cf-cli.rb
@@ -5,10 +5,10 @@ class CfCli < Formula
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
   version '6.32.0'
   if OS.mac?
-    url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.32.0&source=homebrew'
+    url "https://cli.run.pivotal.io/stable?release=macosx64-binary&version=#{version}&source=homebrew"
     sha256 'ddbe83ac8cfe6249431d5a50d5193d127d840fa592261b7050accc2b9757d727'
   elsif OS.linux?
-    url 'https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.32.0&source=homebrew'
+    url "https://cli.run.pivotal.io/stable?release=linux64-binary&version=#{version}&source=homebrew"
     sha256 '0a05521b7198dc8b92efbfb02a8fb04c84eeffeded3387aa3c9eb92ce4abef69'
   end
 
