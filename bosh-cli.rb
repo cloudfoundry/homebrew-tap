af9e7fec55c93e5ce82ab234ff7c345a883a0879
Mark Stokan <markstokan@gmail.com>
2017-10-06 10:08:56 -0700

    Revert "Update formulas to work with linux machines"

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -2,13 +2,8 @@ class BoshCli < Formula
   desc "New BOSH CLI (beta)"
   homepage "https://bosh.io/docs/cli-v2.html"
   version "2.0.41"
-  if OS.mac?
-    url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-    sha256 "609d1deb762d63cf68aef5cd1ea9ddc676ddc2add72892539e123015f5b54232"
-  elsif OS.linux?
-    url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-linux-amd64"
-    sha256 "a9d1278ae1e38b0b54dc2b1f3cb08b4d3af4021d9b934f97265208f86ef6fd92"
-  end
+  url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
+  sha256 "609d1deb762d63cf68aef5cd1ea9ddc676ddc2add72892539e123015f5b54232"
 
   depends_on :arch => :x86_64
 
@@ -16,11 +11,7 @@ class BoshCli < Formula
 
   def install
     binary_name = build.with?("bosh2") ? "bosh2" : "bosh"
-    if OS.mac?
-      bin.install "bosh-cli-#{version}-darwin-amd64" => binary_name
-    elsif OS.linux?
-      bin.install "bosh-cli-#{version}-linux-amd64" => binary_name
-    end
+    bin.install "bosh-cli-#{version}-darwin-amd64" => binary_name
   end
 
   test do
--- credhub-cli.rb
+++ credhub-cli.rb
@@ -2,13 +2,8 @@ class CredhubCli < Formula
   desc "CredHub CLI"
   homepage "https://github.com/cloudfoundry-incubator/credhub-cli"
   version "1.4.1"
-  if OS.mac?
-    url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/#{version}/credhub-darwin-#{version}.tgz"
-    sha256 "7da92f5fec350ff42d1d111a7cfc3d4c254cc086add48a470b6ed255c262b963"
-  elsif OS.linux?
-    url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/#{version}/credhub-linux-#{version}.tgz"
-    sha256 "a9a70389919f3e16e4e0e925d59eb831ba18666a4272f209f6a740fc7a4d6a50"
-  end
+  url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/1.4.1/credhub-darwin-1.4.1.tgz"
+  sha256 "7da92f5fec350ff42d1d111a7cfc3d4c254cc086add48a470b6ed255c262b963"
 
   depends_on :arch => :x86_64
 
