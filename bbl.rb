b25966ad0c8901c2e50e59c085df8c9ef65aef6b
CF Infrastructure <cf-infrastructure@pivotal.io>
2017-11-02 07:51:45 +0000

    Release bbl 5.3.4

--- bbl.rb
+++ bbl.rb
@@ -1,9 +1,9 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
   homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v5.3.3"
+  version "v5.3.4"
   url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
-  sha256 "19c4ac2e34a3fced43749a548233ae94055d308e3322714ca3c965f7c464d53f"
+  sha256 "8ddd3f77574e2bb9a0e93758df1fb59efbcef1d2af737252b8111381613ad480"
 
   depends_on :arch => :x86_64
   depends_on "terraform" => "0.10.0"
