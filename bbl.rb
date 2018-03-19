b1f46f8490a5ff9ccd684027d8b92572fedec29c
CF Infrastructure <cf-infrastructure@pivotal.io>
2018-03-19 23:09:15 +0000

    Release bbl 6.4.4

--- bbl.rb
+++ bbl.rb
@@ -1,19 +1,19 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
   homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v6.4.3"
+  version "v6.4.4"
 
   if OS.mac?
     url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
-    sha256 "601130d376cc5ce903b7688f96c25a0306a527e8ef8aa7387529bee1a31a6151"
+    sha256 "a70d4ba711285e903f66ab5c60bcb345b7170c7d7a63ebbc5f29da49574197e9"
   elsif OS.linux?
     url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_linux_x86-64"
-    sha256 "bf8491941e84dccf778328cb13134da7597205960279dcda25d19613cb8237ed"
+    sha256 "9700c127ed6bfd96f94a61f96d2cc9480ea285277552ca2f018b670a2f56068d"
   end
 
   depends_on :arch => :x86_64
-  depends_on "terraform" => "0.11.0"
-  depends_on "cloudfoundry/tap/bosh-cli" => "2.0.48"
+  depends_on "terraform" => "0.10.0"
+  depends_on "cloudfoundry/tap/bosh-cli" => "2.0.29"
 
   def install
     binary_name = "bbl"
