cae1f52bdc215a13f8f9cfb047eb37b235d17df6
Rahul Jain <rahulkj@gmail.com>
2018-08-22 06:59:03 -0500

    Updating credhub cli release and sha

--- credhub-cli.rb
+++ credhub-cli.rb
@@ -2,8 +2,8 @@ class CredhubCli < Formula
   desc "CredHub CLI"
   homepage "https://github.com/cloudfoundry-incubator/credhub-cli"
   version "1.7.7"
-  url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/1.7.7/credhub-darwin-1.7.7.tgz"
-  sha256 "102e03f672287deeb13f75da6b2a186412ef69f8cd3a9f0a3f2c371f1a830182"
+  url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.0.0/credhub-darwin-2.0.0.tgz"
+  sha256 "2951e134775dd240ca488488fb8f4059b340f9cf0ee4a5555225371ff46a9f8f"
 
   depends_on :arch => :x86_64
 
