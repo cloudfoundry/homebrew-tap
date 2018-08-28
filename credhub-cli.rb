cb597f7dc76b50ef489ceeff3c69120d2932dc98
Rahul Jain <rahulkj@gmail.com>
2018-08-28 16:28:27 -0500

    Update credhub-cli.rb

--- credhub-cli.rb
+++ credhub-cli.rb
@@ -1,7 +1,7 @@
 class CredhubCli < Formula
   desc "CredHub CLI"
   homepage "https://github.com/cloudfoundry-incubator/credhub-cli"
-  version "1.7.7"
+  version "2.0.0"
   url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.0.0/credhub-darwin-2.0.0.tgz"
   sha256 "2951e134775dd240ca488488fb8f4059b340f9cf0ee4a5555225371ff46a9f8f"
 
