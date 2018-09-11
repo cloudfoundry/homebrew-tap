6caec6ab5228b5a206a1b2c6067e524812c5bab2
Concourse CI <cf-cli-eng@pivotal.io>
2018-09-11 23:23:29 +0000

    Release 6.39.0

--- cf-cli.rb
+++ cf-cli.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class CfCli < Formula
   homepage 'https://code.cloudfoundry.org/cli'
-  version '6.38.0'
+  version '6.39.0'
 
   if OS.mac?
     head 'https://packages.cloudfoundry.org/edge?arch=macosx64&source=homebrew'
-    url 'https://packages.cloudfoundry.org/homebrew/cf-6.38.0.tgz'
-    sha256 '0f7f78a9b4cdfc12b373f61269cc86285b6f56f8458538b35a8d38a3437d0ff2'
+    url 'https://packages.cloudfoundry.org/homebrew/cf-6.39.0.tgz'
+    sha256 'ce849831c0792cabe751fea50bd8188fbaca3937d6a847b887234057ae6b2c36'
   elsif OS.linux?
     head 'https://packages.cloudfoundry.org/edge?arch=linux64&source=homebrew'
-    url 'https://packages.cloudfoundry.org/stable?release=linux64-binary&version=6.38.0&source=homebrew'
-    sha256 '846b9f1746c8e9cfaf11891950536fb805b1a02975c99e8c4e02328d40020604'
+    url 'https://packages.cloudfoundry.org/stable?release=linux64-binary&version=6.39.0&source=homebrew'
+    sha256 '11750f3257802ecb9407e99645908904c017816ac451cebd9ec8d266f3d6382f'
   end
 
   depends_on :arch => :x86_64
