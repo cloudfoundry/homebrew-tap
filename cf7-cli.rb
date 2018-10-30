bdb47e0fd5b026de987d84486a4e6d31576ddc2f
Concourse CI <cf-cli-eng@pivotal.io>
2018-10-30 20:58:15 +0000

    Release 7.0.0-beta.1

---
+++ cf7-cli.rb
@@ -0,0 +1,43 @@
+require 'formula'
+
+class Cf7Cli < Formula
+  homepage 'https://code.cloudfoundry.org/cli'
+  version '7.0.0-beta.1'
+
+  if OS.mac?
+    head 'https://packages.cloudfoundry.org/edge?arch=macosx64&source=homebrew'
+    url 'https://packages.cloudfoundry.org/homebrew/cf-7.0.0-beta.1.tgz'
+    sha256 '1347e79060abc720a74f25bafcb6d3f34ecca5585b34c5443c038d4d9e6e8b29'
+  elsif OS.linux?
+    head 'https://packages.cloudfoundry.org/edge?arch=linux64&source=homebrew'
+    url 'https://packages.cloudfoundry.org/stable?release=linux64-binary&version=7.0.0-beta.1&source=homebrew'
+    sha256 '7a392ac890b5f03840fa1703b0a722ce2015a1dc461f34a625f724cf4ca8dfcf'
+  end
+
+  depends_on :arch => :x86_64
+
+  def install
+    bin.install 'cf7'
+    (bash_completion/"cf7-cli").write <<-completion
+# bash completion for Cloud Foundry CLI
+
+_cf-cli() {
+    # All arguments except the first one
+    args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
+    # Only split on newlines
+    local IFS=$'\n'
+    # Call completion (note that the first element of COMP_WORDS is
+    # the executable itself)
+    COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} "${args[@]}"))
+    return 0
+}
+complete -F _cf-cli cf7
+    completion
+    doc.install 'LICENSE'
+    doc.install 'NOTICE'
+  end
+
+  test do
+    system "#{bin}/cf7"
+  end
+end
