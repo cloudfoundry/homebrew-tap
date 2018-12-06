3df95ace2afce2a4b31179382022d79856304db5
CF CredHub CI Pipeline <pcf-security-credhub+ci@pivotal.io>
2018-12-06 17:06:53 +0000

    Update CredHub CLI to 2.2.0

--- credhub-cli.rb
+++ credhub-cli.rb
@@ -1,9 +1,9 @@
 class CredhubCli < Formula
   desc "CredHub CLI"
   homepage "https://github.com/cloudfoundry-incubator/credhub-cli"
-  version "2.1.0"
-  url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.1.0/credhub-darwin-2.1.0.tgz"
-  sha256 "8759a4f364b0a353ab9db19e8adc986fc3109d398ab29e5584c31127e63171ee"
+  version "2.2.0"
+  url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.2.0/credhub-darwin-2.2.0.tgz"
+  sha256 "65943ad1922c26b54874348042a4f13ac2d43e753a7be77b6947a8d542e7f760"
 
   depends_on :arch => :x86_64
 
