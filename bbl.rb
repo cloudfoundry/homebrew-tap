9c00e7caf47164cfb8ae76c35b60370e4e97c129
CF Infrastructure <cf-infrastructure@pivotal.io>
2019-01-25 04:34:32 +0000

    Release bbl 7.0.8

--- bbl.rb
+++ bbl.rb
@@ -1,14 +1,14 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
   homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v7.0.0"
+  version "v7.0.8"
 
   if OS.mac?
     url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
-    sha256 "e051c73e3299a0e534308613e7ce833e20e3348b1489ee2218ffa86de5e7ab21"
+    sha256 "245e1156ff545a1bd155f55d83b83f6df9c96b84c6a784b65355018145bca515"
   elsif OS.linux?
     url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_linux_x86-64"
-    sha256 "3ae287f258f1380e09d8dbe96790491bf3121e6e4111bbe6664a4859d3b6f152"
+    sha256 "838bb66ed6d33b3846257bb7be6b1b10e16334cd8f1c259ae1c0c6669b2bab41"
   end
 
   depends_on :arch => :x86_64
