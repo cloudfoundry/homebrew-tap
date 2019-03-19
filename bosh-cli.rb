392cc6191ace2fced1f558ae6a2531c330cc24f6
tannevaled <tannevaled@users.noreply.github.com>
2019-03-19 19:19:17 +0100

    add the linux missing part

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -2,16 +2,27 @@ class BoshCli < Formula
   desc "New BOSH CLI (beta)"
   homepage "https://bosh.io/docs/cli-v2.html"
   version "5.4.0"
-  url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-  sha256 "82ef492d4e54a7005573c8f0e2f919858832293134caedce7c03f2df0184244e"
-
+  
+  if OS.mac?
+    url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
+    sha256 "82ef492d4e54a7005573c8f0e2f919858832293134caedce7c03f2df0184244e"
+  elsif OS.linux?
+    url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-linux-amd64"
+    sha256 "ecc1b6464adf9a0ede464b8699525a473e05e7205357e4eb198599edf1064f57"
+  end
+  
   depends_on :arch => :x86_64
 
   option "with-bosh2", "Rename binary to 'bosh2'. Useful if the old Ruby CLI is needed."
 
   def install
     binary_name = build.with?("bosh2") ? "bosh2" : "bosh"
-    bin.install "bosh-cli-#{version}-darwin-amd64" => binary_name
+    if OS.mac?
+      bin.install "bosh-cli-#{version}-darwin-amd64" => binary_name
+    elsif OS.linux?
+      bin.install "bosh-cli-#{version}-linux-amd64" => binary_name
+    end
+  
     (bash_completion/"bosh-cli").write <<-completion
       _#{binary_name}() {
           # All arguments except the first one
