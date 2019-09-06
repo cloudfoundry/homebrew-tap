9de184ef461ca0d7d9d97ad192cb618b5fbfdd8d
CF CredHub CI Pipeline <pcf-security-credhub+ci@pivotal.io>
2019-09-06 16:04:52 +0000

    Update CredHub CLI to 2.5.3

--- credhub-cli.rb
+++ credhub-cli.rb
@@ -1,14 +1,14 @@
 class CredhubCli < Formula
   desc "CredHub CLI"
   homepage "https://github.com/cloudfoundry-incubator/credhub-cli"
-  version "2.5.2"
+  version "2.5.3"
 
   if OS.mac?
-    url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.5.2/credhub-darwin-2.5.2.tgz"
-    sha256 "29168645603eab6c921d2792b75a648ce94344b39c813f617b272f85c634e294"
+    url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.5.3/credhub-darwin-2.5.3.tgz"
+    sha256 "03f46790ac2f99ee7bf35cf656026e053fef1a323f04fcbe848965f1064733c9"
   elsif OS.linux?
-    url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.5.2/credhub-linux-2.5.2.tgz"
-    sha256 "8a3e20229afd273b7ebea185ded049fa91fa1c86aa547fd3df4631ac08ef316f"
+    url "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.5.3/credhub-linux-2.5.3.tgz"
+    sha256 "db74c8fa2d73594b84e11a9562927e2c558a2c7454870f8df298347fba946240"
   end
 
   depends_on :arch => :x86_64
