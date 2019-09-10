2428dd9742ffb54f5fd9a628a896d525ccc8e1c8
Concourse CI <cf-cli-eng@pivotal.io>
2019-09-10 23:09:05 +0000

    Release 7.0.0-beta.25

--- cf7-cli.rb
+++ cf7-cli.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Cf7Cli < Formula
   homepage 'https://code.cloudfoundry.org/cli'
-  version '7.0.0-beta.24'
+  version '7.0.0-beta.25'
 
   if OS.mac?
-    url 'https://packages.cloudfoundry.org/homebrew/cf7-7.0.0-beta.24.tgz'
-    sha256 '5ec8162a07851d7fbcc0760dc8ef25bd6d524373645f0c2a400c066785c17839'
+    url 'https://packages.cloudfoundry.org/homebrew/cf7-7.0.0-beta.25.tgz'
+    sha256 'd87412e8030727a7e8935398e492a927cdc7caff8329ae7aef9f8a7dabe5bb22'
   elsif OS.linux?
-    url 'https://packages.cloudfoundry.org/stable?release=linux64-binary&version=7.0.0-beta.24&source=homebrew'
-    sha256 'b8deb0f36b8fe6c7568f784d32d76233c63882f7614fc3a73a31d3b79bfce54a'
+    url 'https://packages.cloudfoundry.org/stable?release=linux64-binary&version=7.0.0-beta.25&source=homebrew'
+    sha256 'cc1f64524f9df1a9d9fbbe88cac1a40fe343387032157785f9872f1f42bb1414'
   end
 
   depends_on :arch => :x86_64
