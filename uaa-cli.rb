71cc6840fddd6d587c90e9d837e4a624e3ddddf8
Joshua Casey <jcasey@pivotal.io>
2020-03-11 14:05:15 -0500

    Fix UAA version typo
    0.1.0 -> 0.10.0

--- uaa-cli.rb
+++ uaa-cli.rb
@@ -1,7 +1,7 @@
 class UaaCli < Formula
   desc "CLI for UAA written in golang"
   homepage "https://github.com/cloudfoundry-incubator/uaa-cli"
-  version "0.1.0"
+  version "0.10.0"
   url "https://github.com/cloudfoundry-incubator/uaa-cli/releases/download/0.10.0/uaa-darwin-amd64-0.10.0"
   sha256 "482cd886a2abd60678c292b126e4c869c1305a610fbe8d05a78207e22edeff88"
 
