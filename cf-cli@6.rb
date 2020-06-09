74a4bf71eebfd7d240b8081d292db083dd31632b
James Palmer <jpalmer@pivotal.io>
2020-06-09 21:37:53 +0000

    Release 6.35.1

--- cf-cli@6.rb
+++ cf-cli@6.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class CfCliAT6 < Formula
   homepage 'https://code.cloudfoundry.org/cli'
-  version '6.35.0'
+  version '6.35.1'
 
   if OS.mac?
     head 'https://packages.cloudfoundry.org/edge?arch=macosx64&source=homebrew'
-    url 'https://packages.cloudfoundry.org/stable?release=macosx64-binary&version=6.35.0&source=homebrew'
-    sha256 '8246ba69285f7aee3a2e2abe0962fb7d4bc3397626cc2cd02a5ea597343a6bab'
+    url 'https://packages.cloudfoundry.org/stable?release=macosx64-binary&version=6.35.1&source=homebrew'
+    sha256 '2f2424f0b843395c6a47442b06d49dbf8605d0cd7c56c8512be3eebc24a4714a'
   elsif OS.linux?
     head 'https://packages.cloudfoundry.org/edge?arch=linux64&source=homebrew'
-    url 'https://packages.cloudfoundry.org/stable?release=linux64-binary&version=6.35.0&source=homebrew'
-    sha256 '9d2c1b73616fa7b22c51d6102dcfa20cd7fa73a2352b697ae180a3922ed3f6e1'
+    url 'https://packages.cloudfoundry.org/stable?release=linux64-binary&version=6.35.1&source=homebrew'
+    sha256 'ab9b8d2b15b3ec215cf829e4585109de510992e3b3db00c9ce4845332bbf16b1'
   end
 
   depends_on :arch => :x86_64
