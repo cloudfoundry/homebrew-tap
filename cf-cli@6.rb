ed455670718cd2f19b2dcbf3683514dee778aaa3
James Palmer <jpalmer@pivotal.io>
2020-06-09 21:37:52 +0000

    Release 6.23.0

--- cf-cli@6.rb
+++ cf-cli@6.rb
@@ -3,8 +3,8 @@ require 'formula'
 class CfCliAT6 < Formula
   homepage 'https://code.cloudfoundry.org/cli'
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
-  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.22.1&source=homebrew'
-  version '6.22.1'
+  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.23.0&source=homebrew'
+  version '6.23.0'
   sha256 '2da08e15223147d87caf51bc7fff6eb224c3a194d57052e69f3bc2ba05209541'
 
   depends_on :arch => :x86_64
