54dc5a7a2a571139b0f324c3b6710ef6ffd69412
BOSH CI <cf-bosh-eng@pivotal.io>
2020-06-17 21:08:46 +0000

    Release bosh-cli 6.3.0

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -1,9 +1,15 @@
 class BoshCli < Formula
-  desc "New BOSH CLI (beta)"
+  desc "BOSH CLI"
   homepage "https://bosh.io/docs/cli-v2.html"
-  version "6.2.1"
-  url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-  sha256 "1d2ced5edc7a9406616df7ad9c0d4e3ade10d66d33e753885ab8e245c037e280"
+  version "6.3.0"
+
+  if OS.mac?
+    url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
+    sha256 "e3f7b2f72b27270fdd360b029ba1e2d7151ac5862e7a398d4fb64d79b09f9cfb"
+  elsif OS.linux?
+    url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-linux-amd64"
+    sha256 "177bf3b65da346b031bdccd39a4495ac89715a41ddd7c207813a22844f49623b"
+  end
 
   depends_on :arch => :x86_64
 
@@ -11,7 +17,11 @@ class BoshCli < Formula
 
   def install
     binary_name = build.with?("bosh2") ? "bosh2" : "bosh"
-    bin.install "bosh-cli-#{version}-darwin-amd64" => binary_name
+    if OS.mac?
+      bin.install "bosh-cli-#{version}-darwin-amd64" => binary_name
+    elsif OS.linux?
+      bin.install "bosh-cli-#{version}-linux-amd64" => binary_name
+    end
     (bash_completion/"bosh-cli").write <<-completion
       _#{binary_name}() {
           # All arguments except the first one
