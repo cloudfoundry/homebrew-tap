cd2a2074a4d6f4114b716b7e80416097f6db6e64
PCF Backup & Restore CI <cf-lazarus@pivotal.io>
2020-09-22 14:49:36 +0000

    Release BBR CLI v1.8.0

--- bbr.rb
+++ bbr.rb
@@ -1,8 +1,8 @@
 class Bbr < Formula
   desc "BOSH Backup and Restore CLI"
   homepage "https://github.com/cloudfoundry-incubator/bosh-backup-and-restore"
-  url "https://github.com/cloudfoundry-incubator/bosh-backup-and-restore/releases/download/v1.7.2/bbr-1.7.2.tar"
-  sha256 "73daa9c245e9ec91611a6a9e2a520d51c625b9a0a566dffff2fa5a66455cfac7"
+  url "https://github.com/cloudfoundry-incubator/bosh-backup-and-restore/releases/download/v1.8.0/bbr-1.8.0.tar"
+  sha256 "6a94ddab99a69f512c7f499377da5ee390583c256c6930e84390fa7b985aba40"
 
   depends_on :arch => :x86_64
 
