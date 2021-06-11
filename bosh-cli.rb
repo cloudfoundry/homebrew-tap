a699fc31633a7c5bebfab1cfbf91667eb8ad1a24
BOSH CI <cf-bosh-eng@pivotal.io>
2021-06-11 20:34:17 +0000

    Release bosh-cli 6.4.4

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -1,14 +1,14 @@
 class BoshCli < Formula
   desc "BOSH CLI"
   homepage "https://bosh.io/docs/cli-v2.html"
-  version "6.4.3"
+  version "6.4.4"
 
   if OS.mac?
     url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-    sha256 "3968c607265fe16acf9cfb6565a56c8e77b8c23b4d9e466ba5bceb4a6dc34ce3"
+    sha256 "77757c3df9a1f2b5e24e30079a5ad6351ecd5b85c938a8b651a17530c18670dd"
   elsif OS.linux?
     url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{version}-linux-amd64"
-    sha256 "8f10eb8830afa4f64889c88e51c1d436849513d4fc99653ad7670bf1df8abff3"
+    sha256 "c944dd694e5470686995c2365ac60c9ef06f655cab51994f7fefed4c89e764fb"
   end
 
   depends_on :arch => :x86_64
