823ef38805a61e7d6f4071ddb33f6dce07dfe7ed
Jhonathan Aristizabal <jhonathana@vmware.com>
2022-01-07 13:06:56 -0500

    Bumped to v8.4.77
    Authored-by: Jhonathan Aristizabal <jhonathana@vmware.com>

--- bbl.rb
+++ bbl.rb
@@ -1,14 +1,14 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
   homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v8.4.74"
+  version "v8.4.77"
 
   if OS.mac?
     url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
-    sha256 "7c689ccc6c8a34f28b24390b616965a7d5a5a72bd8523163ffceaa38e005351f"
+    sha256 "5b1c1bac72794eb5234f0e1451fb4dc3d6e79af2750c08335c6836068eb95f6b"
   elsif OS.linux?
     url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_linux_x86-64"
-    sha256 "49917e2c631241b956adbba8562ef4cbc3669a8609a19ba7153761140e46fda1"
+    sha256 "e7f60d8ae7fbd5624fb2a1721e3516e692dfa2ac5b7b52d61a43bf4a38231a17"
   end
 
   depends_on :arch => :x86_64
