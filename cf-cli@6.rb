c5c013332493a5636eeb6a4f6dcfc1e4c0b5d5ce
Alexander Berez <aberezovsky@vmware.io>
2022-07-12 11:32:53 -0700

    Fix CF CLI v6 Homebrew formula URL

--- cf-cli@6.rb
+++ cf-cli@6.rb
@@ -7,7 +7,7 @@ class CfCliAT6 < Formula
 
   if OS.mac?
     head 'https://packages.cloudfoundry.org/edge?arch=macosx64&source=homebrew'
-    url 'https://packages.cloudfoundry.org/homebrew/cf-6.53.0.tgz'
+    url 'https://packages.cloudfoundry.org/stable?release=macosx64-binary&version=6.53.0&source=homebrew'
     sha256 '3d5f94759dd4aa135fb10e04c6525f2b747727cbb5789ff1a0aacce365fa9515'
   elsif OS.linux?
     head 'https://packages.cloudfoundry.org/edge?arch=linux64&source=homebrew'
