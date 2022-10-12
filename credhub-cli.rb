f465417c5c1aa5d3602e275ac3eabbc32eab8de6
CF CredHub CI Pipeline <pvtl-credhub+ci@vmware.com>
2022-10-12 23:36:11 +0000

    Update CredHub CLI to 2.9.5

--- credhub-cli.rb
+++ credhub-cli.rb
@@ -1,14 +1,14 @@
 class CredhubCli < Formula
   desc "CredHub CLI"
   homepage "https://github.com/cloudfoundry/credhub-cli"
-  version "2.9.4"
+  version "2.9.5"
 
   if OS.mac?
-    url "https://github.com/cloudfoundry/credhub-cli/releases/download/2.9.4/credhub-darwin-2.9.4.tgz"
-    sha256 "200d688d73dd2adf624f9529836055dc7ba1a1c9bdccd0c422c843454b7fe7d5"
+    url "https://github.com/cloudfoundry/credhub-cli/releases/download/2.9.5/credhub-darwin-2.9.5.tgz"
+    sha256 "4bd99fafb367b4f94e70ca4ac44739a14c33176ed4b93411e1431a3dcb61e0d4"
   elsif OS.linux?
-    url "https://github.com/cloudfoundry/credhub-cli/releases/download/2.9.4/credhub-linux-2.9.4.tgz"
-    sha256 "dd20858958fcb5f9546bd51d533d2f34b1e67f7427e2be75a8f57009cd4e2611"
+    url "https://github.com/cloudfoundry/credhub-cli/releases/download/2.9.5/credhub-linux-2.9.5.tgz"
+    sha256 "ecdb581bf09b4218de40af1943609f0815120fd2a1680cee2013fdf938c9a8e0"
   end
 
   depends_on :arch => :x86_64
