7403d686f283c9b23509b9b18c9b8225bdf769a6
PCF Backup & Restore CI <cf-lazarus@pivotal.io>
2023-09-27 11:51:37 +0000

    Release BBR CLI v1.9.51

--- bbr.rb
+++ bbr.rb
@@ -7,15 +7,15 @@ class Bbr < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.50/bbr-1.9.50-darwin-arm64"
-      sha256 "3a35f859cb408b9da7c2c8275815c243584cc90f954f3abd35519edbb0f43ed5"
+      url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.51/bbr-1.9.51-darwin-arm64"
+      sha256 "d0510bb828065c97b96fe4a7ae4f2193fb74a01aa95173735c015a4cfc0af757"
     else
-      url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.50/bbr-1.9.50-darwin-amd64"
-      sha256 "3a35f859cb408b9da7c2c8275815c243584cc90f954f3abd35519edbb0f43ed5"
+      url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.51/bbr-1.9.51-darwin-amd64"
+      sha256 "d0510bb828065c97b96fe4a7ae4f2193fb74a01aa95173735c015a4cfc0af757"
     end
   elsif OS.linux?
-    url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.50/bbr-1.9.50-linux-amd64"
-    sha256 "0a9b262b878bb08f0fcb830170fa1eff92a40e3254ea2a43a473a23c6ea677dd"
+    url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.51/bbr-1.9.51-linux-amd64"
+    sha256 "90415fb88b7717a96fddd9f12e8195314129c746c4b61394e72e9744b1bf6ed0"
   end
 
   def install
@@ -23,12 +23,12 @@ class Bbr < Formula
 
     if OS.mac?
       if Hardware::CPU.arm?
-        bin.install "bbr-1.9.50-darwin-arm64" => binary_name
+        bin.install "bbr-1.9.51-darwin-arm64" => binary_name
       else
-        bin.install "bbr-1.9.50-darwin-amd64" => binary_name
+        bin.install "bbr-1.9.51-darwin-amd64" => binary_name
       end
     elsif OS.linux?
-      bin.install "bbr-1.9.50-linux-amd64" => binary_name
+      bin.install "bbr-1.9.51-linux-amd64" => binary_name
     end
   end
 
@@ -36,4 +36,3 @@ class Bbr < Formula
     system "#{bin}/bbr", "version"
   end
 end
-
