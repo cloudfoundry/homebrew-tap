03921b74380d510315b548525b3932949a79b9a8
PCF Backup & Restore CI <cf-lazarus@pivotal.io>
2023-10-22 07:20:10 +0000

    Release BBR CLI v1.9.53

--- bbr.rb
+++ bbr.rb
@@ -7,15 +7,15 @@ class Bbr < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.51/bbr-1.9.51-darwin-arm64"
-      sha256 "d0510bb828065c97b96fe4a7ae4f2193fb74a01aa95173735c015a4cfc0af757"
+      url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.53/bbr-1.9.53-darwin-arm64"
+      sha256 "6c8d3f021921264dd654914c3f4c86ab15dcf2703754e5de01477caa2cfb863f"
     else
-      url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.51/bbr-1.9.51-darwin-amd64"
-      sha256 "d0510bb828065c97b96fe4a7ae4f2193fb74a01aa95173735c015a4cfc0af757"
+      url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.53/bbr-1.9.53-darwin-amd64"
+      sha256 "6c8d3f021921264dd654914c3f4c86ab15dcf2703754e5de01477caa2cfb863f"
     end
   elsif OS.linux?
-    url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.51/bbr-1.9.51-linux-amd64"
-    sha256 "90415fb88b7717a96fddd9f12e8195314129c746c4b61394e72e9744b1bf6ed0"
+    url "https://github.com/cloudfoundry/bosh-backup-and-restore/releases/download/v1.9.53/bbr-1.9.53-linux-amd64"
+    sha256 "aa10b46e0557f04d43b0329d3d94de1d8b5eb52b1d278b4c9206e2785390b54f"
   end
 
   def install
@@ -23,12 +23,12 @@ class Bbr < Formula
 
     if OS.mac?
       if Hardware::CPU.arm?
-        bin.install "bbr-1.9.51-darwin-arm64" => binary_name
+        bin.install "bbr-1.9.53-darwin-arm64" => binary_name
       else
-        bin.install "bbr-1.9.51-darwin-amd64" => binary_name
+        bin.install "bbr-1.9.53-darwin-amd64" => binary_name
       end
     elsif OS.linux?
-      bin.install "bbr-1.9.51-linux-amd64" => binary_name
+      bin.install "bbr-1.9.53-linux-amd64" => binary_name
     end
   end
 
