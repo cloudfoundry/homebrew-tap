8e17ff5cd8d1900b523384a785aea2470759e098
BOSH CI <cf-bosh-eng@pivotal.io>
2024-08-10 16:22:46 +0000

    Release bosh-cli 7.6.3

--- bosh-cli.rb
+++ bosh-cli.rb
@@ -1,19 +1,19 @@
 class BoshCli < Formula
   desc "BOSH CLI"
   homepage "https://bosh.io/docs/cli-v2.html"
-  version "7.6.2"
+  version "7.6.3"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://storage.googleapis.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-arm64"
-      sha256 "621b51ecb9102c83e68c0a08ca0cf3dcadc12bbaaacc4dcef7d24868eed862c2"
+      sha256 "25b33b0ddaa64d84edf4584aedb2e217f25f14e47cddf1c8a60491013e10db51"
     else
       url "https://storage.googleapis.com/bosh-cli-artifacts/bosh-cli-#{version}-darwin-amd64"
-      sha256 "35acdccd2e462b362ec3892af60c094ee80ebc5eafa9d0e003a97375b04a0de5"
+      sha256 "486497e81c596447e180c5a1189467a46357e5cd4b0c277a6638c64a102434c9"
     end
   elsif OS.linux?
     url "https://storage.googleapis.com/bosh-cli-artifacts/bosh-cli-#{version}-linux-amd64"
-    sha256 "b4acaa01c0703af5de6b8c8044799fbacf43bd9200087d0c34ed25d7f882dc43"
+    sha256 "8ea2b1757a0e2bf66e537ae4314694220f304d733dc6f2efe0a28a1b01c6b832"
   end
 
   option "with-bosh2", "Rename binary to 'bosh2'. Useful if the old Ruby CLI is needed."
