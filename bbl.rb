28e926923550ae10e1c3b8569b1e25810a5f204c
BOSH CI <cf-bosh-eng@pivotal.io>
2026-03-19 18:21:14 +0000

    Release bbl 9.0.42

--- bbl.rb
+++ bbl.rb
@@ -1,29 +1,44 @@
 class Bbl < Formula
   desc "Command line utility for standing up a BOSH director on an IAAS of your choice."
   homepage "https://github.com/cloudfoundry/bosh-bootloader"
-  version "v8.4.103"
+  version "v9.0.42"
 
   if OS.mac?
-    url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx"
-    sha256 "a944525328c20ca3f7251fc52e8c459635bf920033843eeac2a834e8a93a4317"
+    if Hardware::CPU.arm?
+      url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx_arm64"
+      sha256 "5aa224369a1094b6f9d9522c26f8e182de92878dcfa1488d9b1ec81c0423b922"
+    else
+      url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_osx_amd64"
+      sha256 "a391946320228161e4a4c64dd21357ea3126945d550f6ee9248f4f80f907ee3a"
+    end
   elsif OS.linux?
-    url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_linux_x86-64"
-    sha256 "e2d3e69bf4479495cbce44dc8f5cdf28cb0c99f9d5815c3cbf2344d4b27677d6"
+    if Hardware::CPU.arm?
+      url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_linux_arm64"
+      sha256 "ba525d0bc58854c9dca7a25cb7fcd39cd9c8ae8e8ac4728ac645fd2ef1721a00"
+    else
+      url "https://github.com/cloudfoundry/bosh-bootloader/releases/download/#{version}/bbl-#{version}_linux_amd64"
+      sha256 "97fbdbe4be7344f20e4a7afc0f0d07798c1d966a36b85624b665ef738d80a8d1"
+    end
   end
-
-  depends_on "terraform" => "0.11.0"
-  depends_on "cloudfoundry/tap/bosh-cli" => "2.0.48"
 
   def install
     binary_name = "bbl"
     if OS.mac?
-      bin.install "bbl-#{version}_osx" => binary_name
+      if Hardware::CPU.arm?
+        bin.install "bbl-#{version}_osx_arm64" => binary_name
+      else
+        bin.install "bbl-#{version}_osx_amd64" => binary_name
+      end
     elsif OS.linux?
-      bin.install "bbl-#{version}_linux_x86-64" => binary_name
+      if Hardware::CPU.arm?
+        bin.install "bbl-#{version}_linux_arm64" => binary_name
+      else
+        bin.install "bbl-#{version}_linux_amd64" => binary_name
+      end
     end
   end
 
   test do
-    system "#{bin}/#{binary_name} --help"
+    system "#{bin}/bbl", "--help"
   end
 end
